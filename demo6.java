6c5b814ffdef677742e710a66224b240f3dfedf0
Akshitha012908 <akshitharaovelpuri@gmail.com>
2025-01-06 08:14:31 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,10 @@
+public class demo6  {
+    public static void main(String args[]) {
+        String username = "Akshitha";
+        String pwd = "akkie@19";
+        while((username == "Akshitha") && (pwd == "akkie@19")) {
+            System.out.println("hello user Happy new year");
+            break;
+        }
+    }
+}
