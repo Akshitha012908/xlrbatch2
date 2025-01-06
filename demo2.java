43cad4613360920bc39a771403b096d7323b7241
Akshitha012908 <akshitharaovelpuri@gmail.com>
2025-01-06 08:13:12 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,13 @@
+class DEMO2{
+public static void main(String[] args) {
+    int num1,num2;
+    num1=18;
+    num2=15;
+    if(num1>num2){
+        System.out.println("num1 is greatest number");
+    }
+    else{
+        System.out.println("num2 is greatest number");
+    }
+}
+}
